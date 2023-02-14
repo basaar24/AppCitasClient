1cd0487e000f78d18c537c00f44d295720f0a8c0
Arturo Barajas <basaar.24@gmail.com>
2023-02-14 07:27:29 -0600

    Parent to child communication

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,14 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent {
   registerMode = false;
+  users: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-
+    this.getUsers();
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (response: any) => this.users = response,
+      error: (err: Error) => console.log(err),
+      complete: () => console.log('Request has completed')
+    });
   }
 }
