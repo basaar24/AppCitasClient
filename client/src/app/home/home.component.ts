1e34603018e6db156e622844a746c2633b5f4f03
Arturo Barajas <basaar.24@gmail.com>
2023-02-15 07:05:11 -0600

    Child to paren communication

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -27,4 +27,8 @@ export class HomeComponent {
       complete: () => console.log('Request has completed')
     });
   }
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
+  }
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class RegisterComponent {
   @Input() usersFromHomeComponent: any;
+  @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
   constructor() { }
@@ -20,7 +21,7 @@ export class RegisterComponent {
   }
 
   cancel(): void {
-    console.log("Cancelled");
+    this.cancelRegister.emit(false);
   }
 
 }
